0e38d7b8ec737c8a670376decdeec9e4a4f39b43
ShavitIdan <idanshavit5@gmail.com>
2024-12-31 11:28:12 +0200

    setup basic server and db connection

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const logger = require("morgan");
+
+const port = process.env.PORT || 8080;
+
+app.use((req, res) => {
+  res.status(400).send("Page wasn't found");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
